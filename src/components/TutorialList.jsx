36b7e6417322d1553579ad962f10601e23981d2b
Erhan <erhantezer68@gmail.com>
2023-02-27 02:17:25 +0300

    updated tutorial list

--- src/components/TutorialList.jsx
+++ src/components/TutorialList.jsx
@@ -1,9 +1,30 @@
 
 
-const TutorialList = () => {
+const TutorialList = ({tutorials}) => {
     return (
-        <div>TutorialList</div>
+        <div className="container mt-4">
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#id</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Description</th>
+                        <th scope="col" className="text-center">Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tutorials?.map((tutorial) => {
+                        return (
+                            <tr>
+                                
+                            </tr>    
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
 export default TutorialList
+
