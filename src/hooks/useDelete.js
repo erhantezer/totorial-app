356d3befe9a44e57f5b59d08bea04d2ed4714fc3
Erhan <erhantezer68@gmail.com>
2023-02-28 00:22:39 +0300

    delete hook finished

--- src/hooks/useDelete.js
+++ src/hooks/useDelete.js
@@ -1,7 +1,21 @@
+import axios from "axios"
+import { useGlobalContext } from "../context";
 
 
 const useDelete = () => {
-    return {}
+const {getTutorials} = useGlobalContext()
+    const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials";
+
+    const deleteTutorials = async (id) => {
+        try {
+            await axios.delete(`${BASE_URL}/${id}/`)
+        } catch (error) {
+            console.log(error)
+        }
+        getTutorials(); //! database dan sildikten sonra tekrar çağırırız yeniden render edip güncellesin 
+    }
+
+    return { deleteTutorials }
 }
 
 export default useDelete
